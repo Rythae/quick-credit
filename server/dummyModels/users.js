396c8195594337a204932f80091483d43e7440e2
rythae <auxano42@gmail.com>
2019-05-20 00:02:48 +0100

    update to api file

--- server/dummyModels/users.js
+++ server/dummyModels/users.js
@@ -1,32 +1,33 @@
+import uuid from 'uuidv4';
 import bcrypt from 'bcryptjs';
 
 const users = [
   {
-    id: 1,
+    id: uuid(),
     email: 'quickuser1@quick-cred.test',
     firstName: 'Rita',
     lastName: 'Smith',
-    password: bcrypt.hashSync('dummypass123', 10),
+    password: bcrypt.hashSync('dummypass123', Number(process.env.SALT_ROUNDS)),
     address: '5, Smith Road, Smith Town',
     isAdmin: false,
     status: 'verified'
   },
   {
-    id: 2,
+    id: uuid(),
     email: 'quickuser2@quick-cred.test',
     firstName: 'Jane',
     lastName: 'Dough',
-    password: bcrypt.hashSync('dummypass1234', 10),
+    password: bcrypt.hashSync('dummypass1234', Number(process.env.SALT_ROUNDS)),
     address: '7, Smith Road, Smith Town',
     isAdmin: true,
     status: 'verified'
   },
   {
-    id: 3,
+    id: uuid(),
     email: 'quickuser3@quick-cred.test',
     firstName: 'Rita',
     lastName: 'Smith',
-    password: bcrypt.hashSync('dummypass123', 10),
+    password: bcrypt.hashSync('dummypass123', Number(process.env.SALT_ROUNDS)),
     address: '5, Smith Road, Smith Town',
     isAdmin: false,
     status: 'verified'
